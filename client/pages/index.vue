04ae6337f6c5d085fd60eb7c16b96945a202a91f
Aaron Helton <helton@un.org>
2024-09-20 20:13:23 -0400

    debugging and styling

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -40,7 +40,7 @@ export default {
             return d.toISOString().split('T')[0]
         }
         this.date = yesterday()
-        this.selectedDate = this.date.replace(/-/g,"/")
+        this.selectedDate = this.date.replace(/-/g, "/")
     },
     methods: {
         async onSubmit() {
@@ -51,7 +51,7 @@ export default {
                     response.json().then(jsonData => {
                         this.symbolData = jsonData
                     }).then(() => this.isLoading = false)
-                })
+                }).catch(e => console.log(e))
         },
         setDate(value) {
             //selectedDate.value = value
@@ -70,25 +70,29 @@ const state = reactive({
 
 <template>
     <UContainer>
-        
-        <UForm :state="state" @submit="onSubmit">
-            <div class="grid gap-4 grid-cols-3">
-                <UFormGroup label="Duty Station" name="dutyStation">
-                    <UDropdown :items="dutyStations" v-model="dutyStation">
-                        <UButton color="white" :label="dutyStation" trailing-icon="i-heroicons-chevron-down-20-solid" />
-                    </UDropdown>
-                </UFormGroup>
-                <UFormGroup label="Choose a Date" name="date">
-                    <VueDatePicker id="manual" v-model="selectedDate" format="yyyy-MM-dd" :enable-time-picker="false"
-                        @update:model-value="setDate" auto-apply />
-                </UFormGroup>
-                <UButton type="submit">Submit</UButton>
-            </div>
-        </UForm>
-
-        <UTable :loading="isLoading" :loadingstate="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Loading...' }"
-            :progress="{ color: 'primary', animation: 'carousel' }" class="min-w-full" :columns="columns"
-            :rows="symbolData" :ui="{ td: { base: 'max-w-[0] text-balance' } }" />
+        <UCard>
+            <UForm :state="state" @submit="onSubmit">
+                <div class="grid gap-4 grid-cols-3">
+                    <UFormGroup label="Duty Station" name="dutyStation">
+                        <UDropdown :items="dutyStations" v-model="dutyStation">
+                            <UButton color="white" :label="dutyStation"
+                                trailing-icon="i-heroicons-chevron-down-20-solid" />
+                        </UDropdown>
+                    </UFormGroup>
+                    <UFormGroup label="Choose a Date" name="date">
+                        <VueDatePicker id="manual" v-model="selectedDate" format="yyyy-MM-dd"
+                            :enable-time-picker="false" @update:model-value="setDate" auto-apply />
+                    </UFormGroup>
+                    <UButton type="submit"
+                        class="inline-flex items-center justify-center rounded-br-lg border border-transparent mt-5">
+                        Submit</UButton>
+                </div>
+            </UForm>
+        </UCard>
+            <UTable :loading="isLoading"
+                :loadingstate="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Loading...' }"
+                :progress="{ color: 'primary', animation: 'carousel' }" class="min-w-full" :columns="columns"
+                :rows="symbolData" :ui="{ td: { base: 'max-w-[0] text-balance' } }" />
 
         <pre>{{ [dutyStation, date] }}</pre>
     </UContainer>
